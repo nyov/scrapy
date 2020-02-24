3cf7018155208392b70c8334ab64a318a49a0cb8
nyov <nyov@nexnode.net>
2020-02-24 14:01:42 +0000

    [HACK] Allow crawling emoji domains

--- scrapy/_monkeypatches.py
+++ scrapy/_monkeypatches.py
@@ -1,5 +1,17 @@
 import copyreg
 
+try:
+    """ HACK to use python builtin idna lookup for emoji TLD domain names """
+    import idna
+    from idna import encode as iencode, decode as idecode
+    # ccTLD list generated from:
+    # scrapy shell 'https://emojicatch.com/%F0%9F%98%80'
+    # >>> tlds = tuple(response.xpath('//div[@id="results"]/button/p/text()').getall())
+    tlds = ('.ws', '.to', '.ga', '.cf', '.tk', '.ml', '.gq', '.st', '.fm', '.je', '.gg', '.radio.am', '.radio.fm', '.kz', '.com.kz', '.org.kz')
+    tldb = tuple([t.encode() for t in tlds])
+    idna.encode = lambda text: text.encode("idna") if text.endswith(tlds) else iencode(text)
+    idna.decode = lambda octs: octs.decode("idna") if octs.endswith(tldb) else idecode(octs)
+except: pass
 
 # Undo what Twisted's perspective broker adds to pickle register
 # to prevent bugs like Twisted#7989 while serializing requests
